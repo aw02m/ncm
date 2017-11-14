d887a65444ea0cd6251a339c9583d12dd7a75352
aw2mgn <amoh8192@gmail.com>
2017-11-14 14:26:51 +0900

    add rep5-2

--- 5/5-2.c
+++ 5/5-2.c
@@ -1,136 +1,147 @@
 #include<stdio.h>
-#include<math.h>
 #include"matrix.h"
 
 #define N 3
 
-void disassemble(double **A, double **L, double **Lt)
+void disassembleLU(double **a, double **l, double **u)
 {
-  int i, j, k;
+  int i,k,m;
   double sum;
 
-  for (j = 0; j < N; j++) {
-    for(i = 0; i <= j; i++) {
+  for(k=0;k<N;k++){
+    for(i=k;i<N;i++){
+      // L
       sum = 0;
-      if (i != j) {
-        // 非対角成分
-        for (k = 0; k < i; k++) {
-          sum += L[i][k] * L[j][k];
-        }
-        L[j][i] = (A[i][j] - sum) / L[i][i];
-        Lt[i][j] = L[j][i]; // Lt
-      } else {
-        // 対角成分
-        for (k = 0; k < i; k++) {
-          sum += L[i][k] * L[i][k];
-        }
-        L[i][i] = sqrt(A[i][i] - sum);
-        Lt[i][i] = L[i][i];
+      for(m=0;m<k;m++){
+        sum += l[i][m] * u[m][k];
       }
+      l[i][k] = a[i][k] - sum;
+      // U
+      sum = 0;
+      for(m=0;m<k;m++){
+        sum += l[k][m] * u[m][i];
+      }
+      u[k][i] = (a[k][i] - sum) / l[k][k];
     }
   }
 }
 
-// 前回のものを流用
-void solveMatrix(double **L, double **U, double *x, double *y, double *b)
+void solveMatrix(double **L, double **U, double **B, double **X, double **Y)
 {
-  int k, m;
+  int i,k,m;
   double sum;
 
-  // 前進代入 (yを求める)
-  for (k = 0; k < N; k++) {
-    sum = 0;
-    for (m = 0; m <= k; m++) {
-      sum += L[k][m] * y[m];
+  for(i=0;i<N;i++){
+    // 前進代入 (yを求める)
+    for(k=0;k<N;k++){
+      sum = 0;
+      for(m=0;m<=k;m++){
+        sum += L[k][m] * Y[m][i];
+      }
+      Y[k][i] = (B[k][i] - sum) / L[k][k];
     }
-    y[k] = (b[k] - sum) / L[k][k];
-  }
 
-  // 後退代入 (xを求める)
-  for (k = N-1; k >= 0; k--) {
-    sum = 0;
-    for(m = k; m < N; m++) {
-      sum += U[k][m] * x[m];
+    // 後退代入 (xを求める)
+    for(k=N-1;k>=0;k--){
+      sum = 0;
+      for(m=k;m<N;m++){
+        sum += U[k][m] * X[m][i];
+      }
+      X[k][i] = Y[k][i] - sum;
     }
-    x[k] = y[k] - sum;
   }
 }
 
 int main(void)
 {
-  int i, j;
-  double **A, **L, **Lt, **multi;
-  double *b, *x, *y;
+  int i,j;
+  double **A,**L,**U,**B,**X,**Y,**E,**Xi;
 
-  A = matrix(N, N);
-  L = matrix(N, N);
-  Lt = matrix(N, N);
-  multi = matrix(N, N); // L*Ltの確認用
-  b = vector(N);
-  x = vector(N);
-  y = vector(N);
-  
-	A[0][0] = 3.0; A[0][1] = 2.0; A[0][2] = 1.0;
-  A[1][0] = 2.0; A[1][1] = 3.0; A[1][2] = 2.0;
-  A[2][0] = 1.0; A[2][1] = 2.0; A[2][2] = 3.0;
+  A = matrix(N,N);
+  L = matrix(N,N);
+  U = matrix(N,N);
+  B = matrix(N,N);
+  X = matrix(N,N);
+  Y = matrix(N,N);
+  E = matrix(N,N);
+  Xi = matrix(N,N);
 
-	b[0] = 1;
-  b[1] = 3;
-  b[2] = -1;
-
-  for (i = 0; i < N; i++) {
-    for(j = 0; j < N; j++) {      
+  for(i=0;i<N;i++){
+    for(j=0;j<N;j++){
+      A[i][j] = 0;
       L[i][j] = 0;
-      Lt[i][j] = 0;
-      multi[i][j] = 0;
+      U[i][j] = 0;
+      B[i][j] = 0;
+      X[i][j] = 0;
+      Y[i][j] = 0;
+      Xi[i][j] = 0;
+      if(i == j){
+        E[i][j] = 1;
+      }else{
+        E[i][j] = 0;
+      }
     }
-    x[i] = 0;
-    y[i] = 0;
   }
+   
+	A[0][0] = 1; A[0][1] = 1;  A[0][2] = 2;
+  A[1][0] = -2; A[1][1] = 1; A[1][2] = 0;
+  A[2][0] = 1; A[2][1] = 2;  A[2][2] = 3;
 
-  disassemble(A, L, Lt);
-  solveMatrix(L, Lt, x, y, b);
-  multi = multiplyMatrix(N, L, Lt); // L*Ltが正しいか確認
+	B[0][0] = -5; B[0][1] = -2; B[0][2] = 1;
+  B[1][0] = -3; B[1][1] = 4;  B[1][2] = 3;
+  B[2][0] = -5; B[2][1] = -3; B[2][2] = 3;
+
+	disassembleLU(A,L,U);
+  solveMatrix(L,U,E,Xi,Y);
+  solveMatrix(L,U,B,X,Y);
 
   // L
   printf("L\n");
-	for (i = 0; i < N; i++) {
-		for (j = 0; j < N; j++) {
+	for(i=0;i<N;i++){
+		for(j=0;j<N;j++){
 			printf("%f ", L[i][j]);
 		}
 		printf("\n");
 	}
-  // Lt
-  printf("Lt\n");
-	for (i = 0; i < N; i++) {
-		for (j = 0; j < N; j++) {
-			printf("%f ", Lt[i][j]);
+  
+  // U
+	printf("U\n");
+	for(i=0;i<N;i++){
+		for(j=0;j<N;j++){
+			printf("%f ", U[i][j]);
 		}
 		printf("\n");
 	}
-  // L*Lt
-  printf("L*Lt\n");
-  for (i = 0; i < N; i++) {
-		for (j = 0; j < N; j++) {
-			printf("%f ", multi[i][j]);
-		}
-		printf("\n");
-  }
-  // y
-  printf("y\n");
-  for (i = 0; i < N; i++) {
-    printf("%f", y[i]);
+  printf("Y\n");
+
+  // Y
+  for(i=0;i<N;i++){
+    for(j=0;j<N;j++){
+      printf("%f ", Y[i][j]);
+    }
     printf("\n");
   }
-  // x
-  printf("x\n");
-  for (i = 0; i < N; i++) {
-    printf("%f", x[i]);
+  printf("X\n");
+
+  // X
+  for(i=0;i<N;i++){
+    for(j=0;j<N;j++){
+      printf("%f ", X[i][j]);
+    }
+    printf("\n");
+  }
+  printf("Xi\n");
+
+  // Xi
+  for(i=0;i<N;i++){
+    for(j=0;j<N;j++){
+      printf("%f ", Xi[i][j]);
+    }
     printf("\n");
   }
 
-  free(A); free(L); free(Lt);
-  free(b); free(x); free(y);
+  free(A); free(L); free(U); free(B);
+  free(X); free(Y); free(E); free(Xi);
   
 	return 0;
 }
